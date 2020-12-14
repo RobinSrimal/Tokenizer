1cfca56ea949f0316edd18bd31c4c029796d16d9
Robin Srimal <robin.srimal@gmail.com>
2020-12-14 14:58:13 +0100

    api working

--- api.py
+++ api.py
@@ -1,3 +1,36 @@
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from tokenizer import Tokenizer
+from typing import Optional
+
+
+class ToBeTokenized(BaseModel):
+    text: str
+    lang: Optional[str] = None
+
+app = FastAPI()
+
+
+@app.post("/tokenizer")
+async def feature(item: ToBeTokenized):
+
+    item = jsonable_encoder(item)
+
+    if item["lang"] != None:
+
+        tokenizer = Tokenizer(language = item["lang"])
+        tokens = tokenizer.do(item["text"])
+        return tokens
+
+    else: 
+
+        tokenizer = Tokenizer(detect_language=True)
+        tokens = tokenizer.do(item["text"])
+        return tokens
+
+
+
 
 
 
--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,3 @@
-from languages import get_nlp
 from google_trans_new import google_translator  
 
 class Tokenizer:
@@ -27,7 +26,7 @@ class Tokenizer:
             By default detect_language is set to False. 
 
     3. The "do" method returns - given correct input - a dictionary of the form 
-    {"lang": "detected or provided language,
+    {"lang": "detected or provided language",
     "tokens": "created tokens in the form of a list"} 
 
     """
@@ -38,40 +37,84 @@ class Tokenizer:
         self.language = language
         self.detect_language = detect_language
 
-    def get_tokens(self, language, text):
+    def get_nlp(self, language):
 
-        nlp = get_nlp(language)
+        """"
+        this method returns the corresponding spacy language model when 
+        provided with a language. To do so it also does the required 
+        import. This is certainly not the standard approach. 
+        But as this endpoint will be deployed to Heroku (space limitation)
+        and only be invoked rarely it is the fastest approach.
+        """
+
+        if language == "en":
+
+            from spacy.lang.en import English
+            return English()
+
+        elif language == "fr":
+
+            from spacy.lang.fr import French
+            return French()
+
+        elif language == "de":
+
+            from spacy.lang.de import German
+            return German()
+
+        elif language == "es":
+
+            from spacy.lang.es import Spanish
+            return Spanish()
+
+        elif language == "pt":
+
+            from spacy.lang.pt import Portuguese
+            return Portuguese()
+
+        else:
+
+            return {"error": "invalid or not supported language entered"}
+
+
+    def get_tokens(self, text):
+
+        nlp = self.get_nlp(self.language)
         tokenizer = nlp.Defaults.create_tokenizer(nlp)
         tokens = tokenizer(text)
-        # clean punctuation 
-        tokens = [token for token in tokens if not token.is_punct]
+        # clean punctuation and transform tokens object to list
+        tokens = [token.text for token in tokens if not token.is_punct]
 
-            return tokens
+        return tokens
 
     def do(self, text):
 
         if self.detect_language:
 
             detector = google_translator()  
-            language = detector.detect(text)
+            self.language = detector.detect(text)[0]
 
-            if language in ["en","fr","de","es","pt"]:
+            if self.language in ["en","fr","de","es","pt"]:
 
-                tokens = self.get_tokens(language, text)
-        
-                return {"lang": language, "tokens": tokens}
+                tokens = self.get_tokens(text)
+
+                return {"lang": self.language, "tokens": tokens}
 
             else: 
                 return {"error": "detected language is not supported"}
 
-        if self.language:
+        if self.language != None: 
 
             if self.language in ["en","fr","de","es","pt"]:
 
-                tokens = self.get_tokens(self.language, text)
-
-                return {"lang": language, "tokens": tokens}
+                tokens = self.get_tokens(text)
+                return {"lang": self.language, "tokens": tokens}
 
             else: 
                 return {"error": "provided language is not supported"}
+
+
+
+
+
 
